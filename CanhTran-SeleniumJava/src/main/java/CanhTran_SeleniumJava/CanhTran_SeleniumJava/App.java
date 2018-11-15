ec65bed21e72146c3aeef94adcc1349ecbee27b9
Canh Tran <canh.tran@logigear.com>
2018-11-15 15:47:05 +0700

    aa

---
+++ CanhTran-SeleniumJava/src/main/java/CanhTran_SeleniumJava/CanhTran_SeleniumJava/App.java
@@ -0,0 +1,13 @@
+package CanhTran_SeleniumJava.CanhTran_SeleniumJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
