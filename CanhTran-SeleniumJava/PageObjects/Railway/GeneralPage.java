a356573b6091f231ba10e194d62e6dd22e44f57b
canhtranngoc <canh.tran.bm@gmail.com>
2018-11-17 23:14:28 +0700

    abc

---
+++ CanhTran-SeleniumJava/PageObjects/Railway/GeneralPage.java
@@ -0,0 +1,15 @@
+package Railway
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import Constant.Constant;
+
+public class GeneralPage {
+	//Locators
+	private final By tabLogin = By.xpath("")
+	private final By tabLogin = By.xpath("")
+	private final By tabLogin = By.xpath("")
+	
+	
+}
