8c5ac2ddf250502283509abbaa49e64d19900941
canhtranngoc <canh.tran.bm@gmail.com>
2018-11-20 20:28:59 +0700

    addnew

---
+++ CanhTranSeleL1/Common/Common/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Manager
+ *
+ */
+package Common;
---
+++ CanhTranSeleL1/src/main/java/CanhTranSeleL1/CanhTranSeleL1/App.java
@@ -0,0 +1,13 @@
+package CanhTranSeleL1.CanhTranSeleL1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
